8b96ce84351bd7bfaff5b4384fe955cf4b3a6d8e
Dwedia <eric@bellaiche.net>
2025-09-01 12:24:14 +0200

    removed packages, added curl, commented out distrobox role

--- reinstall.sh
+++ reinstall.sh
@@ -5,6 +5,12 @@ DISTRO=$(cat /etc/os-release | grep PRETTY | awk -F '"' '{print $2}' | awk '{pri
 
 echo "Distrobution is $DISTRO, using correct options"
 
+echo ""
+echo "=========================================="
+echo "Installing Ansible if not present"
+echo "=========================================="
+echo ""
+
 # Install ansible-core on Fedora
 if [ "Fedora" = $DISTRO ];
 then
@@ -32,11 +38,23 @@ fi
 # install the community general flatpak module
 ansible-galaxy collection install community.general
 
+echo ""
+echo "=========================================="
+echo "Running ansible playbook"
+echo "=========================================="
+echo ""
+
 # Check if ansible is installed, and if so, run the reinstall playbook.
 if [ -f /usr/bin/ansible ]; 
 then
   ansible-playbook reinstall-playbook.yml -K;
 fi
+
+echo ""
+echo "=========================================="
+echo "Setting git global settings"
+echo "=========================================="
+echo ""
 
 # Ask for git username and email and put them in variable
 read -p "Enter your Git username: " gitUserName
@@ -54,6 +72,12 @@ else
   sleep 5
 fi
 
+echo ""
+echo "=========================================="
+echo "Setting firewall rules"
+echo "=========================================="
+echo ""
+
 # Set up firewall with firewalld if Distribution is Fedora
 if [ "Fedora" = $DISTRO ];
 then
@@ -69,3 +93,5 @@ then
   sudo ufw default allow outgoing
   sudo ufw enable
 fi
+
+read -p "Setup done, Reboot now? (y/n): " confirm && [[ $confirm == "y" ]] && reboot
